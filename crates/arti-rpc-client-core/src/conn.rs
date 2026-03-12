b3bcd9469bf58ead32e43dbc886c98cd6812087a
Nick Mathewson <nickm@torproject.org>
2026-03-12 09:36:40 -0400

    rpc: Rename PollingStream → BlockingConnection.

--- crates/arti-rpc-client-core/src/conn.rs
+++ crates/arti-rpc-client-core/src/conn.rs
@@ -750,7 +750,7 @@ mod test {
 
     use crate::{
         msgs::request::{JsonMap, Request, ValidatedRequest},
-        nb_stream::PollingStream,
+        nb_stream::BlockingConnection,
     };
 
     use super::*;
@@ -758,7 +758,7 @@ mod test {
     /// helper: Return a dummy RpcConn, along with a socketpair for it to talk to.
     fn dummy_connected() -> (RpcConn, crate::testing::SocketpairStream) {
         let (s1, s2) = crate::testing::construct_socketpair().unwrap();
-        let conn = RpcConn::new(PollingStream::new(s1).unwrap());
+        let conn = RpcConn::new(BlockingConnection::new(s1).unwrap());
 
         (conn, s2)
     }
