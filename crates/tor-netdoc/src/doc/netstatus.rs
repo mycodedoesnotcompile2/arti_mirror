d006593ba014721bb5e2adeac4df65f2f770d2e6
Ian Jackson <ijackson@chiark.greenend.org.uk>
2026-05-18 16:25:33 +0100

    tor-netdoc: implement encoding for consensus in votes' `m` items
    We will test this when we test round trip parsing/encoding of votes.
    For now, mark it as incomplete.

--- crates/tor-netdoc/src/doc/netstatus.rs
+++ crates/tor-netdoc/src/doc/netstatus.rs
@@ -92,6 +92,7 @@ use void::ResultVoidExt as _;
 
 use derive_deftly::{Deftly, define_derive_deftly};
 use digest::Digest;
+use itertools::Itertools;
 use std::sync::LazyLock;
 use tor_checkable::{ExternallySigned, timed::TimerangeBound};
 use tor_llcrypto as ll;
@@ -316,6 +317,15 @@ pub mod consensus_methods_comma_separated {
             }
         }
         Ok(ConsensusMethods { methods })
+    }
+
+    /// Encode
+    #[cfg(feature = "incomplete")] // untested
+    pub fn write_arg_onto(self_: &ConsensusMethods, out: &mut ItemEncoder) -> Result<(), Bug> {
+        for s in Itertools::intersperse(self_.methods.iter().map(|v| v as &dyn Display), &",") {
+            out.args_raw_string(s);
+        }
+        Ok(())
     }
 }
 
