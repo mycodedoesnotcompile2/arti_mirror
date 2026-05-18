b25729975d9b619842c82139b47a9d837794eb92
Ian Jackson <ijackson@chiark.greenend.org.uk>
2026-05-18 16:21:38 +0100

    tor-netdoc: derive Ord for RouterStatusMdDigestsVote
    Sorting by the applicable consensus methods set seems reasonable.
    
    The spec doesn't state the order for this.  I think that's fine.
    We can't expect to repro the same consensus with different software,
    and we will produce stable output.

--- crates/tor-netdoc/src/doc/netstatus/rs.rs
+++ crates/tor-netdoc/src/doc/netstatus/rs.rs
@@ -63,7 +63,7 @@ static OTHER_VERSION_CACHE: InternCache<str> = InternCache::new();
 ///    must sort it.
 ///  * These non-invariants apply both within one instance of this struct,
 ///    and across multiple instances of it within a `RouterStatus`.
-#[derive(Debug, Clone, Default, Eq, PartialEq, Deftly)]
+#[derive(Debug, Clone, Default, Eq, PartialEq, Ord, PartialOrd, Deftly)]
 #[derive_deftly(ItemValueParseable)]
 #[non_exhaustive]
 pub struct RouterStatusMdDigestsVote {
