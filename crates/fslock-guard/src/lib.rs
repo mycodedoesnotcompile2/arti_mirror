826ab5ec80f82326dc75979db3b0654bdb137d79
Nick Mathewson <nickm@torproject.org>
2026-05-27 14:39:48 -0400

    fslock-guard: Provide specialized android impls.
    We need these because `File::lock()` and `File::try_lock()`
    claim not to work on Android, and have to be emulated with flock.
    For more information, see
    <https://github.com/rust-lang/rust/issues/148325>.
    
    Closes #2544.
    
    Based on code by @syphyr.
    
    Co-Authored-By: syphyr <syphyr@gmail.com>

--- crates/fslock-guard/src/lib.rs
+++ crates/fslock-guard/src/lib.rs
@@ -165,6 +165,67 @@ fn do_try_lock(f: &fs::File) -> Result<(), std::fs::TryLockError> {
     f.try_lock()
 }
 
+/// Try to lock `f`, blocking if need be.
+///
+/// On android, we need to use flock manually, since Rust (as of May 2026)
+/// always returns "not implemented" for `lock()` and `try_lock()`.
+///
+/// See <https://github.com/rust-lang/rust/issues/148325>.
+/// Apparently,
+/// although there are filesystems (specifically FUSE filesystems)
+/// where flock won't work, it will correctly report ENOSYS
+/// on those filesystems.
+//
+// TODO MSRV ????: we can remove this once Rust supports file locking on Android
+// at our MSRV.  As of May 2026, https://github.com/rust-lang/rust/pull/157038/
+// seems like the likeliest MR for that, but it has not been merged.
+#[cfg(target_os = "android")]
+fn do_lock(f: &fs::File) -> std::io::Result<()> {
+    use std::os::fd::AsRawFd;
+
+    let fd = f.as_raw_fd();
+    // SAFETY: Since `f` is a file, it has a valid fd.
+    let success = unsafe { libc::flock(fd, libc::LOCK_EX) } == 0;
+
+    if success {
+        Ok(())
+    } else {
+        Err(std::io::Error::last_os_error())
+    }
+}
+
+/// Try to lock `f`, without blocking.
+///
+/// On android, we need to use flock manually, since Rust (as of May 2026)
+/// always returns "not implemented" for `lock()` and `try_lock()`.
+///
+/// See <https://github.com/rust-lang/rust/issues/148325>.
+/// Apparently,
+/// although there are filesystems (specifically FUSE filesystems)
+/// where flock won't work, it will correctly report ENOSYS
+/// on those filesystems.
+//
+// TODO MSRV ????: See 'TODO MSRV' on do_lock above.
+#[cfg(target_os = "android")]
+fn do_try_lock(f: &fs::File) -> Result<(), std::fs::TryLockError> {
+    use std::os::fd::AsRawFd;
+
+    let fd = f.as_raw_fd();
+    // SAFETY: Since `f` is a file, it has a valid fd.
+    let success = unsafe { libc::flock(fd, libc::LOCK_EX | libc::LOCK_NB) } == 0;
+
+    if success {
+        Ok(())
+    } else {
+        let err = std::io::Error::last_os_error();
+        if err.kind() == std::io::ErrorKind::WouldBlock {
+            Err(std::fs::TryLockError::WouldBlock)
+        } else {
+            Err(std::fs::TryLockError::Error(err))
+        }
+    }
+}
+
 /// An error that we return when the path given to `delete_lock_file` does not
 /// match the file we have.
 ///
