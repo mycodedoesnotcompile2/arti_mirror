28434bbe2a372354824b8424743e3ce8738bef46
Ian Jackson <ijackson@chiark.greenend.org.uk>
2026-06-04 15:07:48 +0100

    tor-checkable: TimerangeBound: is_valid: Treat bounds as inclusive
    This makes more sense because:
    
     * `start_bound` and `end_bound` return `Bound::Included`
     * It doesn't make sense to say that a document isn't valid at
       the very start of its validity period.  (And we don't want a half-open range.)
     * NotYetValid(0ns) and Expired(0ns) are rather nonsensical errors.
    
    In real timeliness tests it will make no difference since the current
    time is only equal to the bound for a nanosecond.  But tests that like
    to test corner cases might be affected, and possibly some synthetic
    situations (where the document's own validity time start is used
    somehow) might change.

--- crates/tor-checkable/src/timed.rs
+++ crates/tor-checkable/src/timed.rs
@@ -185,13 +185,13 @@ impl<T> crate::Timebound<T> for TimerangeBound<T> {
     fn is_valid_at(&self, t: &time::SystemTime) -> Result<(), Self::Error> {
         use crate::TimeValidityError;
         if let Some(start) = self.start {
-            if let Ok(d) = start.duration_since(*t) {
+            if let Ok(d) = start.duration_since(*t) && d > time::Duration::ZERO {
                 return Err(TimeValidityError::NotYetValid(d));
             }
         }
 
         if let Some(end) = self.end {
-            if let Ok(d) = t.duration_since(end) {
+            if let Ok(d) = t.duration_since(end) && d > time::Duration::ZERO {
                 return Err(TimeValidityError::Expired(d));
             }
         }
