a6481a8f637555098f51028e1c2c52e38e184e1f
Ian Jackson <ijackson@chiark.greenend.org.uk>
2026-07-21 16:51:41 +0100

    arti-dirauth: Build an arti consensus method plugin binary
    This just panics, right now.

---
+++ crates/arti-dirauth/src/authority_plugin.rs
@@ -0,0 +1,10 @@
+//! Arti consensus method plugin for C Tor
+//!
+//! Implements `authority-plugin.md` pursuant to `doc/dev/notes/dirauth-sketch.md`.
+//!
+//! This is the actual implementation.
+
+/// Entrypoint for the Arti-in-C-Tor consensus method plugin
+pub fn plugin_main() {
+    panic!("nothing is implemented here yet!");
+}
