09a8dfcba8068ed19b1610085d1f3392b2b7a43c
Wesley Aptekar-Cassels <me@wesleyac.com>
2026-09-03 16:41:14 -0400

    onionperf: Add more detailed doccomments.
    And fix typo.

--- crates/tor-basic-utils/src/onionperf_types.rs
+++ crates/tor-basic-utils/src/onionperf_types.rs
@@ -1,15 +1,18 @@
 //! Types representing events that are emitted to enable onionperf.
 //!
-//! These are intended to reflect the C Tor meanings of the similar events.
+//! These are intended to reflect the C Tor meanings of the similar events,
+//! see [asynchronous events spec] for more details, as well as arti#173.
+//!
+//! [asynchronous events spec]: https://spec.torproject.org/control-spec/replies.html#asynchronous-events
 
 // It would be nice to include various IDs in these enums,
 // but doing so would create a circular dependency,
-// as the crates that define those types depend on rot-basic-utils.
+// as the crates that define those types depend on tor-basic-utils.
 #[derive(Debug)]
 #[non_exhaustive]
 /// An event that is intended to be ingested by onionperf.
 pub enum OnionperfEvent {
-    /// A stream-related event.
+    /// A stream-related event (`STREAM` in C Tor)
     ///
     /// Note that this somewhat conflates SOCKS proxy streams and data streams,
     /// as C Tor does the same.
@@ -17,9 +20,9 @@ pub enum OnionperfEvent {
     /// These can be distinguished, as the data streams have a stream ID set in
     /// the emitted events, while the SOCKS streams do not.
     Stream(OnionperfStreamStatus),
-    /// A circuit-related event.
+    /// A circuit-related event (`CIRC` in C Tor).
     Circuit(OnionperfCircuitStatus),
-    /// A guard-related event.
+    /// A guard-related event (`GUARD` in C Tor).
     Guard(OnionperfGuardStatus),
 }
 
@@ -27,11 +30,11 @@ pub enum OnionperfEvent {
 #[non_exhaustive]
 /// A stream status that is intended to be ingested by onionperf.
 pub enum OnionperfStreamStatus {
-    /// A stream was newly created.
+    /// A stream was newly created (`NEW` in C Tor).
     New,
-    /// A stream was closed.
+    /// A stream was closed (`CLOSED` in C Tor).
     Closed,
-    /// A stream has failed and been closed due to an error.
+    /// A stream has failed and been closed due to an error (`FAILED` in C Tor).
     Failed,
 }
 
@@ -39,15 +42,15 @@ pub enum OnionperfStreamStatus {
 #[non_exhaustive]
 /// A circuit status that is intended to be ingested by onionperf.
 pub enum OnionperfCircuitStatus {
-    /// A new circuit was built.
+    /// A new circuit was built (`BUILT` in C Tor).
     Built,
-    /// This tunnel is now usable.
+    /// This tunnel is now usable (`LAUNCHED` in C Tor).
     Launched,
-    /// This circuit was successfully extended by another hop.
+    /// This circuit was successfully extended by another hop (`EXTENDED` in C Tor).
     Extended,
-    /// This circuit was closed, either cleanly or due to an error.
+    /// This circuit was closed, either cleanly or due to an error (`CLOSED` in C Tor).
     Closed,
-    /// This tunnel was not successfully created.
+    /// This tunnel was not successfully created (`FAILED` in C Tor).
     Failed,
 }
 
@@ -55,12 +58,12 @@ pub enum OnionperfCircuitStatus {
 #[non_exhaustive]
 /// A guard status that is intended to be ingested by onionperf.
 pub enum OnionperfGuardStatus {
-    /// A new guard has been added to the set of available guards.
+    /// A new guard has been added to the set of available guards (`NEW` in C Tor).
     New,
-    /// This guard has been used successfully.
+    /// This guard has been used successfully (`UP` in C Tor).
     Up,
-    /// We attempted to use this guard but were not successful.
+    /// We attempted to use this guard but were not successful (`DOWN` in  C Tor).
     Down,
-    /// This guard was removed from the set of available guards.
+    /// This guard was removed from the set of available guards (`DROPPED` in C Tor).
     Dropped,
 }
