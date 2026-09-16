5ebdaf480bdfac906b32c8de6451b30c1d67dfca
Gabriela Moldovan <gabi@torproject.org>
2026-09-16 09:41:54 +0000

    arti-relay: Implement dns::handle_resolve()
    Closes #2696

--- crates/arti-relay/src/stream/dns.rs
+++ crates/arti-relay/src/stream/dns.rs
@@ -2,9 +2,12 @@
 
 pub(crate) mod resolver;
 
-use resolver::{DnsResolver, LookupAnswers, LookupError};
+use resolver::{DnsResolver, LookupAnswers, LookupError, RecordData};
 
-use tor_proto::stream::IncomingStream;
+use tor_cell::relaycell::msg::Resolved;
+use tor_proto::stream::{IncomingStream, IncomingStreamRequest};
+
+use tracing::trace;
 
 /// The fake TTL to send back with every RESOLVED response.
 ///
@@ -17,8 +20,38 @@ const FAKE_TTL_SECONDS: u32 = 60;
 /// Handle an incoming DNS stream
 #[allow(clippy::unused_async)] // TODO(relay)
 pub(crate) async fn handle_resolve(
-    _incoming: IncomingStream,
-    resolver: DnsResolver,
+    incoming: IncomingStream,
+    mut resolver: DnsResolver,
 ) -> anyhow::Result<()> {
-    todo!()
+    let resolve = match incoming.request() {
+        IncomingStreamRequest::Resolve(r) => r,
+        s => return Err(anyhow::anyhow!("expected RESOLVE but got {s:?}")),
+    };
+    let res = resolver.resolve(resolve.query()).await;
+
+    let resolved = lookup_to_resolved(res);
+    trace!(cell=?resolved, "sending RESOLVED");
+
+    incoming.resolve(resolved).await?;
+
+    Ok(())
+}
+
+/// Build a [`Resolved`] message out of a lookup response.
+///
+/// Each of the returned answers will have the TTL set to a fake hard-coded value,
+/// to avoid leaking the DNS cache insertion timestamps to the client.
+//
+// TODO(relay): we might want to return random fake TTLs instead
+fn lookup_to_resolved(lookup_res: Result<LookupAnswers<RecordData>, LookupError>) -> Resolved {
+    match lookup_res {
+        Ok(answers) => {
+            let mut resolved = Resolved::new_empty();
+            for ans in answers {
+                resolved.add_answer(ans.into(), FAKE_TTL_SECONDS);
+            }
+            resolved
+        }
+        Err(e) => Resolved::new_err(e.is_transient(), FAKE_TTL_SECONDS),
+    }
 }
